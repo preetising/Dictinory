8d10294d27f548d42872dd7486dc25994425a3da
preetising <84845068+preetising@users.noreply.github.com>
2021-10-10 13:50:40 +0530

    Add files via upload

---
+++ creat_dict.py
@@ -0,0 +1,4 @@
+# a={"brand":"Ford","model":"Mustang","year":1964}
+# print(a)
+
+
---
+++ logical.py
@@ -0,0 +1,31 @@
+# dict={'a':1,'b':2,'c':3,'d':4}
+# total=1
+# for i in dict:
+#     total=total*dict[i]
+# print(total)
+
+# for i in range(1,10):
+
+
+l = ["priya","preeti","shahnaaz","amla","neha"]
+for words in l:
+    c=len(words)
+    if c>5:
+        print({words})
+    
+
+
+# l = ["shnaaz","rani","pooja"]
+# for word in l:
+#     a=len(word)
+#     if a==5:
+#         print(word)
+
+
+# list = ["priya","preeti","shahnaaz","amla","neha"]
+# i=0
+# while i<len(list):
+#     a=len(list)
+#     if a<=5:
+#         print(list)
+#     i=i+1
