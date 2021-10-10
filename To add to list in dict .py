f681933d4b1dc23e8e1aff5168bd599314930af7
preetising <84845068+preetising@users.noreply.github.com>
2021-10-10 13:51:43 +0530

    Add files via upload

---
+++ To add to list in dict .py
@@ -0,0 +1,11 @@
+# a=[1,2,3,4,5]
+# b=["priya","anisha","ujala","anzum","durga"]
+# i=0
+# y={}
+# while i<len(a):
+#     y.update({a[i]:b[i]})
+#     i=i+1
+# print(y)
+
+
+ 
---
+++ update_dict.py
@@ -0,0 +1,39 @@
+# dict={1:"rubi",2:"durga",3:"ujala"}
+# print("orginal dict")
+# print(dict)
+# print(id(dict))
+# dict.update({105:"python",12:"yakufa"})
+# print("update dict")
+# print(dict)
+# print(id(dict))
+
+
+# """*****update in dict********"""
+# dict={1:"rubi",2:"durga",3:"ujala"}
+# print("orginal_dict")
+# print(dict)
+# dict.update({105:"preeti singh",'food':"i love to eat fast food"})
+# print("update_dict")
+# print(dict)
+
+
+
+
+# details={'Name': 'RAM','Age': 17, 'student': {'id': 22,'place': 'dharamsala'}} 
+# details['student']['id']=35
+# print(details); 
+
+
+# details={'Name': 'RAM','Age': 17, 'pooja': {'id': 22,'place': 'dharamsala'}} 
+# details['pooja']['id']=90
+# print(details)
+
+
+
+# person= {'1': 'RAM', '2': 17,}
+# person[3] = 'male'
+# print(person)
+
+name={'8':"sonu","2":18}
+name[2]="Rohan"
+print(name)
