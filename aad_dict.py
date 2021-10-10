77e74adb2048e5d64eb9c9b3dbf5a96f08ca1a52
preetising <84845068+preetising@users.noreply.github.com>
2021-10-10 13:49:45 +0530

    Add files via upload

---
+++ aad_dict.py
@@ -0,0 +1,10 @@
+# dict={'name':'nisha','age':'17'}
+# dict['ORGANIZATION']="navgurukul"
+# dict['place']="noida"
+# print(dict)
+
+
+dict={1:"_bhawna",2:"Rani",5:"MEGHA"}
+dict['name']="navgurkul"
+dict['place']="huskur"
+print(dict)
